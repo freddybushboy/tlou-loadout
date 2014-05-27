0d3f27ed28180f91bd79f0e795e29d76e3972c0d
freddybushboy <f.parke@catchdigital.com>
2014-05-27 14:04:09 +0100

    basic layout setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function LoadoutCalc($scope) {
+  $scope.slotName = 'Survival Skill 4';
+  $scope.remainingPoints = '4';
+
+  $scope.getRepeat = function(num) {
+    return new Array(num);
+  }
+
+}
